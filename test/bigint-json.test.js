62cc2a67c2c95aa3d5a00509c6d93804c9050d7d
David <davidmoller1221@gmail.com>
2020-05-18 13:54:57 +0200

    bugfix: fixed crashes when object contained null

--- test/bigint-json.test.js
+++ test/bigint-json.test.js
@@ -4,30 +4,54 @@ const bigintJSON = require('../index');
 
 
 describe('Tests for bigint json', () => {
+    let object;
+    let stringified;
+    beforeEach(() => {
+        object = {
+            someKey: 'someString',
+            someKey2: 1234,
+            someKey3: 1234n,
+            someKey4: {
+                nestedKey: 'someString',
+                nestedKey2: 1234,
+                nestedKey3: 1234n,
+                nestedKey4: {
+                    nestedNestedKey: {
+                        nestedNestedKey: 'someString',
+                        nestedNestedKey2: 1234,
+                        nestedNestedKey3: 12349912312323891374291847012983471209384712098347012983741029834710298374102983741092837412093847120938471023n,
+                        nestedNestedKey5: null,
+                        nestedNestedKey6: undefined,
+                        nestedNestedKey7: () => {},
+                        nestedNestedKey8: true,
+                        nestedNestedKey9: false
+                    }
+                },
+                nestedKey5: null,
+                nestedKey6: undefined,
+                nestedKey7: () => {},
+                nestedKey8: true,
+                nestedKey9: false
+            },
+            somekey5: null,
+            somekey6: undefined,
+            somekey7: () => {},
+            somekey8: true,
+            somekey9: false
+        };
+        stringified = '{"someKey":"someString","someKey2":1234,"someKey3":"1234n","someKey4":{"nestedKey":"someString","nestedKey2":1234,"nestedKey3":"1234n","nestedKey4":{"nestedNestedKey":{"nestedNestedKey":"someString","nestedNestedKey2":1234,"nestedNestedKey3":"12349912312323891374291847012983471209384712098347012983741029834710298374102983741092837412093847120938471023n","nestedNestedKey5":null,"nestedNestedKey8":true,"nestedNestedKey9":false}},"nestedKey5":null,"nestedKey8":true,"nestedKey9":false},"somekey5":null,"somekey8":true,"somekey9":false}';
+    });
 
     describe('parse', () => {
         it('Should parse as json', () => {
-            let stringified = '{"someKey":"someString","someKey2":1234,"someKey3":"1234n","somekey4":{"nestedKey":"someString","nestedKey2":1234,"nestedKey3":"1234n","nestedKey4":{"nestedNestedKey":{"nestedNestedKey":"someString","nestedNestedKey2":1234,"nestedNestedKey3":"12349912312323891374291847012983471209384712098347012983741029834710298374102983741092837412093847120938471023n"}}}}';
             let parsed = bigintJSON.parse(stringified);
-
-            let expectedObject = {
-                someKey: 'someString',
-                someKey2: 1234,
-                someKey3: 1234n,
-                somekey4: {
-                    nestedKey: 'someString',
-                    nestedKey2: 1234,
-                    nestedKey3: 1234n,
-                    nestedKey4: {
-                        nestedNestedKey: {
-                            nestedNestedKey: 'someString',
-                            nestedNestedKey2: 1234,
-                            nestedNestedKey3: 12349912312323891374291847012983471209384712098347012983741029834710298374102983741092837412093847120938471023n,
-                        }
-                    }
-                }
-            };
-            expect(parsed).to.eql(expectedObject);
+            delete(object.somekey6);
+            delete(object.somekey7);
+            delete(object.someKey4.nestedKey6);
+            delete(object.someKey4.nestedKey7);
+            delete(object.someKey4.nestedKey4.nestedNestedKey.nestedNestedKey6);
+            delete(object.someKey4.nestedKey4.nestedNestedKey.nestedNestedKey7);
+            expect(parsed).to.eql(object);
         });
 
         it('throw error if input is not string', () => {
@@ -43,28 +67,10 @@ describe('Tests for bigint json', () => {
 
     describe('stringify', () => {
         it('should not mutate input object', () => {
-            let object = {
-                someKey: 'someString',
-                someKey2: 1234,
-                someKey3: 1234n,
-                someKey4: {
-                    nestedKey: 'someString',
-                    nestedKey2: 1234,
-                    nestedKey3: 1234n,
-                    nestedKey4: {
-                        nestedNestedKey: {
-                            nestedNestedKey: 'someString',
-                            nestedNestedKey2: 1234,
-                            nestedNestedKey3: 12349912312323891374291847012983471209384712098347012983741029834710298374102983741092837412093847120938471023n,
-                        }
-                    }
-                }
-            };
             let someKey3 = object.someKey3;
             let nestedKey3 = object.someKey4.nestedKey3;
             let nestedNestedKey3 = object.someKey4.nestedNestedKey3;
-            let stringified = bigintJSON.stringify(object);
-
+            bigintJSON.stringify(object);
             expect(someKey3).to.equal(object.someKey3);
             expect(nestedKey3).to.equal(object.someKey4.nestedKey3);
             expect(nestedNestedKey3).to.equal(object.someKey4.nestedNestedKey3);
@@ -79,25 +85,8 @@ describe('Tests for bigint json', () => {
         });
 
         it('Should be able to stringify with bigints to JSON', () => {
-            let object = {
-                someKey: 'someString',
-                someKey2: 1234,
-                someKey3: 1234n,
-                somekey4: {
-                    nestedKey: 'someString',
-                    nestedKey2: 1234,
-                    nestedKey3: 1234n,
-                    nestedKey4: {
-                        nestedNestedKey: {
-                            nestedNestedKey: 'someString',
-                            nestedNestedKey2: 1234,
-                            nestedNestedKey3: 12349912312323891374291847012983471209384712098347012983741029834710298374102983741092837412093847120938471023n,
-                        }
-                    }
-                }
-            };
-            let stringified = bigintJSON.stringify(object);
-            expect(stringified).to.equal('{"someKey":"someString","someKey2":1234,"someKey3":"1234n","somekey4":{"nestedKey":"someString","nestedKey2":1234,"nestedKey3":"1234n","nestedKey4":{"nestedNestedKey":{"nestedNestedKey":"someString","nestedNestedKey2":1234,"nestedNestedKey3":"12349912312323891374291847012983471209384712098347012983741029834710298374102983741092837412093847120938471023n"}}}}')
+            let newStringified = bigintJSON.stringify(object);
+            expect(newStringified).to.equal(stringified)
         });
     });
 });
